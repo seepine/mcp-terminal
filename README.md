e30aa9d042581e6d13beb063014b8cd2c15e5148
seepine <seepine@outlook.com>
2026-02-28 17:00:09 +0800

    chore: tag 0.1.0

--- README.md
+++ README.md
@@ -1,64 +1,29 @@
-# Mcp Server Template
+# Terminal Mcp Server
 
-## Dev
+提供执行命令mcp，方便 openclaw、picoclaw 等各机器人使用
 
-### 1. Install dependencies
+> 请注意，命令执行有一定风险，请尽量在沙箱环境或你的机器人部署在 docker 容器中使用，避免造成损失
 
-```bash
-pnpm install
+## Tools
+
+- get_systeminfo: 获取系统信息
+- exec_command: 执行命令
+- get_datetime: 获取当前时间
+
+## Mcp Config
+
+```json
+{
+  "mcpServers": {
+    "mcp-terminal": {
+      "type": "stdio",
+      "command": "npx",
+      "args": ["-y", "@seepine/mcp-terminal"],
+      // 可选，设置默认执行目录
+      "env": {
+        "DEFAULT_CWD": "/Users/your_name/workspace"
+      }
+    }
+  }
+}
 ```
-
-### 2. Start sse server
-
-```bash
-pnpm dev
-```
-
-### 3. Start mcp inspector
-
-```bash
-pnpm debug
-```
-
-### 4. Test
-
-test your mcp server in chrome of inspector
-
-## Deploy stdio
-
-### 1. Build
-
-```bash
-pnpm build
-```
-
-### 2. Deploy to npm
-
-```bash
-pnpm publish
-```
-
-### 3. Run the server
-
-```bash
-npx {mcp-server-template}
-```
-
-## Deploy docker
-
-### 1. Build
-
-```bash
-docker build -t mcp-server-template .
-```
-
-### 2. Run the server
-
-```bash
-docker run -p 3000:3000 mcp-server-template
-```
-
-### 3. Connect to the server
-
-- type: streamableHttp
-- url: http://localhost:3000/mcp
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,38 +1,143 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { z } from 'zod'
+import systeminformation from 'systeminformation'
+import { spawn } from 'node:child_process'
 
 export function createServer(): McpServer {
   const server = new McpServer({
-    name: 'Weather MCP Server',
+    name: 'Terminal Mcp Server',
     version: '0.1.0',
   })
 
+  const getSysteminfoOutputSchema = z.object({
+    osInfo: z
+      .object({
+        platform: z.string().describe('os platform').optional(),
+        distro: z.string().describe('os distro').optional(),
+        codename: z.string().describe('os codename').optional(),
+        release: z.string().describe('os release').optional(),
+        arch: z.string().describe('os arch').optional(),
+        hostname: z.string().describe('os hostname').optional(),
+      })
+      .optional(),
+    cpu: z
+      .object({
+        speed: z.number().describe('cpu speed').optional(),
+        cores: z.number().describe('cpu cores').optional(),
+        manufacturer: z.string().describe('cpu manufacturer').optional(),
+        brand: z.string().describe('cpu brand').optional(),
+        currentLoad: z.string().describe('cpu current load, like 10.0%').optional(),
+      })
+      .optional(),
+    mem: z
+      .object({
+        total: z.number().describe('mem total').optional(),
+        free: z.number().describe('mem free').optional(),
+        used: z.number().describe('mem used').optional(),
+        active: z.number().describe('mem active').optional(),
+        available: z.number().describe('mem available').optional(),
+        buffers: z.number().describe('mem buffers').optional(),
+        cached: z.number().describe('mem cached').optional(),
+      })
+      .optional(),
+  })
   server.registerTool(
-    'get_weather',
+    'get_systeminfo',
     {
-      description: 'Get weather info for a given city.',
-      inputSchema: {
-        city: z.string().describe('city name'),
-      },
+      description: 'get system info, about osInfo, cpuInfo and memInfo',
+      inputSchema: z.object({
+        fields: z
+          .array(z.enum(['os', 'memory', 'cpu']))
+          .describe('fields to get, default is all')
+          .optional(),
+      }),
+      outputSchema: getSysteminfoOutputSchema,
     },
-    async ({ city }) => {
-      if (!city) {
-        throw new Error('city name is required.')
+    async ({ fields = ['os', 'memory', 'cpu'] }) => {
+      // 获取系统信息
+      const systemInfo: z.infer<typeof getSysteminfoOutputSchema> = {}
+      const task: (() => Promise<void>)[] = []
+      if (fields.includes('os')) {
+        task.push(async () => {
+          systemInfo.osInfo = {
+            ...(await systeminformation.osInfo()),
+          }
+        })
       }
-
-      const weather = {
-        city: city,
-        temperature: Math.floor(Math.random() * 30),
-        condition: 'Sunny',
+      // 获取内存信息
+      if (fields.includes('memory')) {
+        task.push(async () => {
+          systemInfo.mem = {
+            ...(await systeminformation.mem()),
+          }
+        })
       }
-
+      // 获取cpu信息
+      if (fields.includes('cpu')) {
+        task.push(async () => {
+          systemInfo.cpu = {
+            ...(await systeminformation.cpu()),
+            currentLoad: (await systeminformation.currentLoad()).currentLoad.toFixed(2) + '%',
+          }
+        })
+      }
+      await Promise.all(task.map((t) => t()))
+      const result = getSysteminfoOutputSchema.parse(systemInfo)
       return {
-        content: [
-          {
-            type: 'text',
-            text: JSON.stringify(weather, null, 2),
-          },
-        ],
+        content: [{ type: 'text', text: JSON.stringify(result, null, 2) }],
+        structuredContent: result,
+      }
+    },
+  )
+
+  server.registerTool(
+    'get_datetime',
+    {
+      description: 'get current datetime, like 2026/2/28 16:20:51',
+    },
+    async () => {
+      const text = new Date().toLocaleString()
+      return {
+        content: [{ type: 'text', text }],
+      }
+    },
+  )
+
+  server.registerTool(
+    'exec_command',
+    {
+      description: 'exec command in terminal',
+      inputSchema: z.object({
+        command: z.string().describe('command to exec, like ls'),
+        args: z.array(z.string()).describe('args to exec').optional(),
+        cwd: z.string().describe('exec command in this directory').optional(),
+        timeout: z.number().describe('timeout in ms, default is 10000').optional(),
+        env: z.record(z.string(), z.string()).describe('env to exec').optional(),
+      }),
+    },
+    async ({ command, args = [], timeout = 10000, env, cwd }) => {
+      const ls = spawn(command, args, {
+        timeout,
+        cwd: cwd || process.env['DEFAULT_CWD'],
+        env: {
+          ...process.env,
+          ...env,
+        },
+      })
+      let text = ''
+      await new Promise((resolve) => {
+        ls.stdout.on('data', (data) => {
+          text += `${data}`
+        })
+        ls.stderr.on('data', (data) => {
+          text += `${data}`
+        })
+        ls.on('close', (code) => {
+          resolve(code)
+        })
+      })
+      return {
+        content: [{ type: 'text', text }],
       }
     },
   )
